a93385426fa5ac484bb2ac5b86d3cffd37fc9070
Ciaran McKenna <ciarantsc@gmail.com>
2018-08-13 00:11:32 +0100

    Add project files.

---
+++ Navigation/Navigation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+namespace ArdourUploader
+{
+    public class Navigation
+    {
+        AutoItX3 autoIt = new AutoItX3();
+        private string windowName;
+
+        public void HighlightSession()
+        {
+            autoIt.MouseMove(500, 375);
+            Thread.Sleep(2000);
+            autoIt.MouseClick("primary"); // Highlights the first session
+        }
+
+        public void OpenSession()
+        {
+            autoIt.MouseMove(800, 560);
+            autoIt.MouseClick("primary");
+        }
+
+        public void StartSession()
+        {
+            autoIt.MouseClick("primary", 800, 235, 2);
+        }
+
+        public void SelectAll()
+        {
+            autoIt.Sleep(4000);
+            windowName = autoIt.WinGetTitle("[ACTIVE]");
+            autoIt.WinActivate(windowName);
+
+            autoIt.Sleep(2000);
+
+            autoIt.Send("{g}");
+
+            autoIt.Send("{CTRLDOWN}");
+            autoIt.Send("{a}");
+            autoIt.Send("{CTRLUP}");
+        }
+
+        public void ExportTrack()
+        {
+            autoIt.Send("{ALTDOWN}"); // opens export window
+            autoIt.Send("{e}");
+            autoIt.Send("{ALTUP}");
+
+            autoIt.MouseClick("primary", 1100, 85); // Opens export presets
+            autoIt.MouseClick("primary", 1100, 95); // Selects export template (AutomatedExport)
+
+            autoIt.MouseMove(850, 230); // highlights output path
+            autoIt.MouseClick("primary");
+
+            autoIt.Send("{CTRLDOWN}"); // set's file output path (Desktop)
+            autoIt.Send("{a}");
+            autoIt.Send("{CTRLUP}");
+            autoIt.Send(@"C:\Users\ciara\OneDrive\Desktop\ArdourTracks\");
+            autoIt.Send("{ENTER}");
+            autoIt.Send("{ENTER}");
+
+        }
+
+    }
+}
---
+++ OpenProgram/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Win32.TaskScheduler;
+using AutoItX3Lib;
+
+namespace ArdourUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Navigation nav = new Navigation();
+
+            //TaskService ts = new TaskService();
+            //TaskDefinition td = ts.NewTask();
+
+            //td.Principal.RunLevel = TaskRunLevel.Highest;
+            //td.Triggers.AddNew(TaskTriggerType.Logon);
+            //string program_path = @"C:\Program Files\Ardour5\bin\Ardour.exe"; 
+
+            //td.Actions.Add(new ExecAction(program_path, null));
+            //ts.RootFolder.RegisterTaskDefinition("bootArdour", td);
+
+            //Opens the application without adminstrative rights
+            Process ardour = new Process();
+
+            ardour.StartInfo.FileName = @"C:\Program Files\Ardour5\bin\Ardour.exe";
+            ardour.Start();
+
+            nav.HighlightSession();
+            nav.OpenSession();
+            nav.StartSession();
+            nav.SelectAll();
+            nav.ExportTrack();
+        }
+    }
+}
